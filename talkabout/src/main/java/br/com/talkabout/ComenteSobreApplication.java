844af197e6e336868a202e0644f0e7b03bb4ab02
Sergio <sergioadm120@gmail.com>
2023-11-27 12:51:39 -0300

    first commit

---
+++ talkabout/src/main/java/br/com/talkabout/ComenteSobreApplication.java
@@ -0,0 +1,12 @@
+package br.com.talkabout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComenteSobreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ComenteSobreApplication.class, args);
+	}
+
+}
