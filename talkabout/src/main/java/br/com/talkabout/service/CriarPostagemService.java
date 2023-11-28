f6428e65644419d73b8edad8fe22ab74b9bb45aa
Sergio <sergioadm120@gmail.com>
2023-11-28 09:57:17 -0300

    feat: add user and topic logic validatation when creating a comment

--- talkabout/src/main/java/br/com/talkabout/service/CriarPostagemService.java
+++ talkabout/src/main/java/br/com/talkabout/service/CriarPostagemService.java
@@ -3,15 +3,30 @@ package br.com.talkabout.service;
 import br.com.talkabout.data.Postagem;
 import br.com.talkabout.data.Topico;
 import br.com.talkabout.data.Usuario;
+import br.com.talkabout.repository.TopicoRepository;
+import br.com.talkabout.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Service
+@AllArgsConstructor
 public class CriarPostagemService {
+    UsuarioRepository usuarioRepository;
+    TopicoRepository topicoRepository;
     public ModelAndView criarPostagem(@RequestParam Usuario usuario, @RequestParam Topico topico) {
         ModelAndView mav = new ModelAndView("add-post");
         Postagem novaPostagem = new Postagem();
+
+        if (usuarioRepository.findById(usuario.getId()).isEmpty()) {
+            throw new RuntimeException("Usuario não encontrado");
+        }
+
+        if (topicoRepository.findById(topico.getId()).isEmpty()) {
+            throw new RuntimeException("Topico não encontrado");
+        }
+
         novaPostagem.setUsuario(usuario);
         novaPostagem.setTopico(topico);
         mav.addObject("postagem", novaPostagem);
